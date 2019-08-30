a756d8cc8aeb725de33970ff4faaf4fb5cc8feaf
diaslilian <lilian.dias25@gmail.com>
2019-08-30 18:10:39 -0300

    feature: Config appointments.

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,12 +21,17 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.get('/providers', ProviderController.index);
-
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
