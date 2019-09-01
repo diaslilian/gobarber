88191ef16b95fb08b25bacfacfac45dc6f3ee547
diaslilian <lilian.dias25@gmail.com>
2019-09-01 20:45:05 -0300

    commit "feature: Config handlebars and nodemailer."

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '20f3f3fad28428',
+    pass: '33e618071128af',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
